303b30b90dfe2e47c7514acb2c437fe4eabae74f
cygnus33065 <cygnus_33065@yahoo.com>
2020-12-16 17:41:32 -0500

    first anagram problem

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,8 +1,19 @@
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  let letters = str1.split('');
+  for (let i = 0; i < str2.length; i++){
+     const letter = str2[i];
+     const index = letters.indexOf(letter)
+     if (index > -1){
+      letters.splice(index,1)
+     }
+  }
+  if (letters.length === 0){
+    return true
+  }else return false
 }
 
-
+console.log(firstAnagram("gizmo", "sally"));    // => false
+console.log(firstAnagram("elvis", "lives"));  // true
 
 function secondAnagram(str1, str2) {
   // Code goes here ....
